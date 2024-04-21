0f0df05b6a741d84d0ef5fe423fa430181bfc7c4
Abdi <abdullahia0215@gmail.com>
2024-04-21 16:54:58 -0500

    search function takes in both artist and year and returns matching results in an array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -14,24 +14,25 @@ function addToCollection(collection, title, artist, yearPublished) {
 }
 
 console.log(
-  addToCollection(myCollection, "1sampleTitle", "sampleArtist", "sampleYear")
+  addToCollection(myCollection, "1sampleTitle", "sampleArtist", 2024)
 );
 console.log(
-  addToCollection(myCollection, "2sampleTitle", "sampleArtist", "sampleYear")
+  addToCollection(myCollection, "2sampleTitle", "sampleArtist", 2024)
 );
 console.log(
-  addToCollection(myCollection, "3sampleTitle", "sampleArtist", "sampleYear")
+  addToCollection(myCollection, "3sampleTitle", "sampleArtist", 2024)
 );
 console.log(
-  addToCollection(myCollection, "4sampleTitle", "sampleArtist", "sampleYear")
+  addToCollection(myCollection, "4sampleTitle", "sampleArtist", 2024)
 );
 console.log(
-  addToCollection(myCollection, "5sampleTitle", "sampleArtist", "sampleYear")
+  addToCollection(myCollection, "5sampleTitle", "sampleArtist", 2024)
 );
 console.log(
-  addToCollection(myCollection, "6sampleTitle", "sampleArtist", "sampleYear")
+  addToCollection(myCollection, "6sampleTitle", "sampleArtist", 2024)
 );
 
+console.log(addToCollection(myCollection, "Ray Song", "Ray Charles", 1957));
 
 console.log(myCollection);
 
@@ -57,20 +58,23 @@ function findByArtist(collection, artist) {
 console.log(findByArtist(myCollection, "sampleArtist"));
 console.log(findByArtist(myCollection, "Baby Keem"));
 
-// const searchCriteria = { artist: 'Ray Charles', year: 1957 }
+const searchCriteria = { artist: "Ray Charles", year: 1957 };
 
-// function searchArtistAndYear(collection, searchCriteria){
-//   let mostCriteriaArray = [];
-//   for (let i=0; i < collection.length; i++)
-//   if (searchCriteria === collection.album){
-//     mostCriteriaArray.push(collection[i])
-//   }
-//     return mostCriteriaArray;
+function searchArtistAndYear(collection, searchCriteria) {
+  let mostCriteriaArray = [];
+  for (let i = 0; i < collection.length; i++) {
+    if (
+      searchCriteria.artist === collection[i].artist &&
+      searchCriteria.year === collection[i].yearPublished
+    ) {
+      mostCriteriaArray.push(collection[i]);
+    }
+  }
+  return mostCriteriaArray;
+}
 
+console.log(searchArtistAndYear(myCollection, searchCriteria));
 
-// }
-
-// console.log(searchrtistAndYear(myCollection, ``))
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
 // as a lil' chunk of friendly code that you don't need to understand right now.
 // (It's used for automated testing.)
